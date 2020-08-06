1d872827087b63df08e66398b4b41113a0bd8460
VasilyevSergey <vasilyev.s@rambler.ru>
2020-08-06 15:04:25 +0300

    ДЗ 16: исправления

--- homework-16/src/main/java/com/otus/homework/Main.java
+++ homework-16/src/main/java/com/otus/homework/Main.java
@@ -3,12 +3,9 @@ package com.otus.homework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
 @EnableMongoRepositories
-@Import(RepositoryRestMvcConfiguration.class)
 @EnableConfigurationProperties
 @SpringBootApplication
 public class Main {

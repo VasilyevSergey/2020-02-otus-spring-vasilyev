3efa21ec9130f6f656f41e4e39d738d2f5baad28
VasilyevSergey <vasilyev.s@rambler.ru>
2020-08-13 22:57:43 +0300

    ДЗ 18: Hystrix dashboard

--- homework-18/src/main/java/com/otus/homework/Main.java
+++ homework-18/src/main/java/com/otus/homework/Main.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongoRepositories
 @EnableConfigurationProperties
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
